c31cd9951d62d31557e5b94f12f7c3861649515a
ogabek_norpulatov <ogabeknorpulatov571@gmail.com>
2025-10-16 18:41:51 +0500

    cors config

--- src/main/java/com/ogabek/edunova/config/SecurityConfig.java
+++ src/main/java/com/ogabek/edunova/config/SecurityConfig.java
@@ -1,8 +1,8 @@
-package com.ogabek.edunova.security;
+package com.ogabek.edunova.config;
 
+import com.ogabek.edunova.security.JwtAuthFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -15,6 +15,7 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -56,7 +57,13 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "http://localhost:4200"));
+        configuration.setAllowedOrigins(List.of(
+                "https://edunova-production.up.railway.app",
+                "http://localhost:3000",
+                "http://localhost:4200",
+                "http://localhost:8080",
+                "*"
+        ));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
